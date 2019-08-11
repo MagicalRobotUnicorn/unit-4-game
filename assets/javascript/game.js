3a5cd4602bbd7dd03f1525309ad4f0175955bf96
John Weber <johnhenryweber@gmail.com>
2019-08-11 02:47:04 -0700

    Current Enemy Fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,3 @@
-// Game Logic
-
-// Card Classes
-// Each Hero has a photo, attack and HP
-
-
-// Character Class
-
-
-
-// Character Selection
-
-
 var $characterSelection = $('#characterSelection');
 var $enemyArea = $('#enemyArea');
 var $currentEnemy = $('#currentEnemy');
@@ -40,12 +27,6 @@ var darthMaul = new Character('Darth Maul', 180, 25, "darthMaul");
 var characters = [obiWan, lukeSkywalker, bobaFett, darthMaul];
 var playerCharacter;
 var enemyCharacter;
-
-// var enemyCharacters = [];
-// var remainingCharacters = characters.length;
-// var playerCharater;
-// var deadCharacters = [];
-
 
 function updateCharacters() {
   for (var i = 0; i < characters.length; i++) {
@@ -78,16 +59,22 @@ function prepareEnemy() {
   for (var i = 0; i < characters.length; i++) {
     if (i != playerCharacter) {
       var subString = characters[i].characterName.substring(0, (characters[i].characterName.indexOf(" ")));
+
+      // $('#' + i + 'buttonArea').html('<button class="chooseEnemyButton" id="' + i + '">Fight ' + subString + '</button>');
       $('#' + i + '.selectButton').addClass('chooseEnemyButton');
       $('#' + i + '.chooseEnemyButton').removeClass('selectButton');
-      $('#' + i + '.chooseEnemyButton').html('<button id="' + i + '">Fight ' + subString + '</button>');
+      $('#' + i + '.chooseEnemyButton').text('Fight ' + subString);
+      // $('#' + i + '.chooseEnemyButton').html('<button id="' + i + '">Fight ' + subString + '</button>');
+    }
+    else {
+      $('#' + i + '.buttonArea').html('');
     }
   }
 }
 
 function chooseEnemy(index) {
   console.log(index);
-  $('#currentEnemy').append($('#' + index + '.character'));
+  $currentEnemy.append($('#' + index + '.character'));
 }
 
 
@@ -107,7 +94,7 @@ function prepareGame() {
     $genericCharacter.find('#secondCol' + i).append('<div class="characterAttack" id="' + i + '">');
     $genericCharacter.find('#secondCol' + i).append('<div class="buttonArea" id="' + i + '">');
 
-    $('#characterSelection').append($genericCharacter);
+    $characterSelection.append($genericCharacter);
 
   }
 }
@@ -122,86 +109,32 @@ $(document).ready(function () {
   updateCharacters();
   prepareSelection();
 
-
-  $('.selectButton').on('click', function () {
-    playerCharacter = $(this).attr('id');
-    createEnemies();
-    prepareEnemy();
+  $('button').on('click', function () {
+    if (this.classList.contains('chooseEnemyButton')) {
+      enemyCharacter = $(this).attr('id');
+      chooseEnemy(enemyCharacter);
+    }
+    else if (this.classList.contains('selectButton')) {
+      playerCharacter = $(this).attr('id');
+      createEnemies();
+      prepareEnemy();
+    }
   });
 
-  $('.chooseEnemyButton').on('click', function () {
-    enemyCharacter = $(this).attr('id');
-    chooseEnemy(enemyCharacter);
-  });
+  // $('.chooseEnemyButton').on('click', function () {
+  //   console.log("Hello World");
+  //   enemyCharacter = $(this).attr('id');
+  //   chooseEnemy(enemyCharacter);
+  // });
+
+  // $('.selectButton').on('click', function () {
+  //   playerCharacter = $(this).attr('id');
+  //   createEnemies();
+  //   prepareEnemy();
+  // });
+
 
 });
-
-// Update On click events for preparation
-
-// Push characters or save index of player character (add class?)
-
-
-
-  // <div class="character" id="0">
-  //     <div class="row">
-  //       <div class="col-md-6">
-  //         <img src="./assets/images/darthMaul.jpg" class="characterImage">
-  //       </div>
-  //       <div class="col-md-6">
-  //         <div class="characterName">Boba Fett</div>
-  //         <div class="characterHealth" id="0">Health: 150/150</div>
-  //         <div class="characterAttack" id="0">Attack Power: 20</div>
-  //         <div class="characterButton"><button>Attack</button></div>
-  //       </div>
-  //     </div>
-  // </div>
-
-
-//   console.log("1. Obi Wan");
-//   console.log("2. Luke Skywalker");
-//   console.log("3. Boba Fett");
-//   console.log("4. Darth Maul");
-
-//   // var choice = prompt("What number character do you want?");
-
-//   choice -= 1;
-
-//   for (var i = 0; i < characters.length; i++) {
-//     if (i != choice){
-//       enemyCharacters.push(characters[i]);
-//     }
-//   }
-
-//   playerCharacter = characters[choice];
-//   remainingCharacters--;
-
-//   // Choice of enemy
-
-//   for (var i= 0; i < remainingCharacters; i++){
-//     console.log(i + ". " + enemyCharacters[i]);
-//   }
-
-//   // choice = prompt("Who would you like to fight?");
-
-
-
-//     // Character Selection
-//     // Load each character with a class of character, and a specific val or id
-//     // that corresponds to character name
-
-//     // var chooseCharacter = function() {
-
-//     // }
-//     // While character is alive
-//       // Enemy Selection
-//       // Combat
-
-//     // Win or Loss
-
-
-//   }
-
-// );
 
 
 
